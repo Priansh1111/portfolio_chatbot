ad266c7cad15c88dfff7c12dbb7c9110fd6ad54d
Priansh Shetty <prianshetty06@gmail.com>
2026-07-14 18:51:06 +0530

    pdf chunking

---
+++ ingestion/ingest.py
@@ -0,0 +1,9 @@
+import pdfplumber
+import re
+with pdfplumber.open(r"C:\Users\shett\Downloads\resume.pdf") as pdf:
+    first_page = pdf.pages[0]
+    text = first_page.extract_text(x_tolerance = 1.5)
+    text = text.replace("(cid:136)","-")
+    text = re.sub(r"(\w)-\n(\w)", r"\1\2",text)
+    print(text)
+    print(len(pdf.pages))
